a5791bb9eebf798714a91f25e3e95ac1d4c2252a
ldt0909 <[lidt9@uw.edu]>
2018-03-04 22:39:20 -0800

    add interview question draft

---
+++ Interviewquestion.py
@@ -0,0 +1,9 @@
+# Udacity Technical interview 
+# by Flora Li 
+
+# Question 1:
+def anagram(string1,string2):
+	# check if string 1 and string 2 are anagram
+	for i in string1:
+		if i in string2:
+
